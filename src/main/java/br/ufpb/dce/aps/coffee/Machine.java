0bce1f2f05b428e93a0d61ab29691571043ce072
Andersoonalves <anderson.alves@dce.ufpb.br>
2014-05-31 19:08:35 -0300

    Carregamento e Descarga de Moedas

--- src/main/java/br/ufpb/dce/aps/coffee/Machine.java
+++ src/main/java/br/ufpb/dce/aps/coffee/Machine.java
@@ -1,5 +1,23 @@
 package br.ufpb.dce.aps.coffee;
 
 public class Machine {
+	
+	private int[] coins = new int[6];
+
+	public void loadCoins(int numPenny, int numNickel, int numDime,
+			int numQuarter, int numHalfDollar, int numDollar) {
+		
+		coins[0] = numPenny;
+		coins[1] = numNickel;
+		coins[2] = numDime;
+		coins[3] = numQuarter;
+		coins[4] = numHalfDollar;
+		coins[5] = numDollar;
+	}
+
+	public int[] unloadCoins() {
+		return coins;
+	}
+	
 
 }
--- src/test/java/br/ufpb/dce/aps/coffee/MachineTest.java
+++ src/test/java/br/ufpb/dce/aps/coffee/MachineTest.java
@@ -1,6 +1,8 @@
 package br.ufpb.dce.aps.coffee;
 
 
+
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,6 +11,17 @@ import org.junit.Test;
 public class MachineTest {
 
 	@Test
-	public void testApp() {
+	public void loadAndUnloadCoins() {
+		Machine machine = new Machine();
+		machine.loadCoins(0, 10, 10, 5, 5, 0);
+		int[] num = machine.unloadCoins();
+		Assert.assertEquals(0, num[0]);
+		Assert.assertEquals(10, num[1]);
+		Assert.assertEquals(10, num[2]);
+		Assert.assertEquals(5, num[3]);
+		Assert.assertEquals(5, num[4]);
+		Assert.assertEquals(0, num[5]);
 	}
+	
+	
 }
